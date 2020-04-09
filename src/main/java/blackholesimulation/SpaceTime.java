ae75278c0dfabecc593b0ba097b5cf96bc704e28
Maria Vitória de Matos Bezerra <49373532+vitoriamatos@users.noreply.github.com>
2020-04-08 23:17:43 -0300

    Implementation of main

--- src/main/java/blackholesimulation/SpaceTime.java
+++ src/main/java/blackholesimulation/SpaceTime.java
@@ -9,6 +9,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class SpaceTime extends Application {
+	
+	private static final Point2D SPEED= new Point2D(3,5);
+    private static final Point2D POSITION = new Point2D(2,10);
+    private static final double MASS= 1000;
+    
+    
 
 	public static void main(String[] args) {
 		launch(args);
@@ -16,12 +22,25 @@ public class SpaceTime extends Application {
 
 	@Override
 	public void start(Stage stage) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("HomeScreen.fxml"));
+		  /*Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
-	}
-
+        
+        */
+        int cont=0;
+        
+        BlackHole blackHole = new BlackHole(new Point2D(10,10),100000);
+        
+        Asteroid asteroid = new Asteroid(POSITION, SPEED, MASS);
+       
+        while(cont<10){
+        blackHole.pull(asteroid);
+        cont++;
+        }
+       
+       
+    }
 }
