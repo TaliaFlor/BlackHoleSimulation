c6719df4ec5f5e86dd6d83aaba8df842314a6f3e
Maria Vitória de Matos Bezerra <49373532+vitoriamatos@users.noreply.github.com>
2020-04-08 23:15:30 -0300

    Implementation of Pull method

--- src/main/java/blackholesimulation/space/BlackHole.java
+++ src/main/java/blackholesimulation/space/BlackHole.java
@@ -2,6 +2,8 @@ package blackholesimulation.space;
 
 import javafx.geometry.Point2D;
 
+
+
 /**
  * 
  * <p>
@@ -13,6 +15,9 @@ import javafx.geometry.Point2D;
  *
  */
 public class BlackHole {
+	public static final double G = 6.67408*Math.pow(10, -11);
+        private static final double DELTA_TIME=0.1;
+
 
 	private Point2D position;
 	private double mass;
@@ -88,11 +93,73 @@ public class BlackHole {
 	
 	
 	
-	public void pull(Object object) {
-		
-		// TODO: implementation
-		
-	}
+	public void pull(MassiveBody body) {
+       
+
+        /*
+      p5.Vector.sub(this.pos, photon.pos);        
+     const theta = force.heading();
+    const r = force.mag();
+    const fg = G * this.mass / (r * r);
+        
+    let deltaTheta = -fg * (dt / c) * sin(photon.theta - theta);
+    deltaTheta /= abs(1.0 - 2.0 * G * this.mass / (r * c * c));
+    photon.theta += deltaTheta;
+    
+    photon.vel = p5.Vector.fromAngle(photon.theta);
+   
+    photon.vel.setMag(c); --  ta faltando
+         */ 
+        
+        
+        final Point2D force = this.position.subtract(body.getPosition()); // Vetor de direção do buraco negro 
+        final double direction = force.angle(this.position);
+        final double displacement = force.magnitude(); // r 
+        final double gravitationalForce = (G*this.mass)/(displacement*displacement);
+       
+      
+        
+        
+        
+        double bodyDirection = body.getPosition().subtract(this.position).angle(body.getPosition());
+        double deltaDirection = (-gravitationalForce * DELTA_TIME* Math.sin(bodyDirection-direction))/Photon.SPEED; // a direção que o vetor a ponta, formando um  entre o x caartesiano e o vetor princiapl  
+       
+       
+        
+        
+        deltaDirection /= Math.abs(Math.pow(displacement, -1)-this.rs );
+        
+        double bodyMagnitude = body.getPosition().subtract(this.position).magnitude();
+        bodyDirection += deltaDirection;
+        body.setSpeed(createVector(bodyDirection,bodyMagnitude));  
+        
+        System.out.println("body position:"+body.getPosition());
+        System.out.println("body Direction:"+bodyDirection);
+        
+      
+        if (displacement <= this.rs + 0.5) {
+                System.out.println("STOP");
+                return;
+        }
+
+    }
+    
+
+    private Point2D createVector(double angle, double magnitude){
+        double x =(Math.cos(angle)*magnitude*180)/Math.PI;
+        double y =(Math.sin(angle)*magnitude*180)/Math.PI;
+        
+        return new Point2D(x,y);
+        
+       
+        
+        
+        
+        
+    }
+     public double getDeltaTime() {
+        return DELTA_TIME;
+    }
 	
 	public double getPhotonSphere() { return rs * 1.5; }
 
