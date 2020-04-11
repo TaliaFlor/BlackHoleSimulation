35379595c4cc57a3fa955683ac4c1e3f83cfaed3
Maria Vitória de Matos Bezerra <49373532+vitoriamatos@users.noreply.github.com>
2020-04-11 14:39:05 -0300

    Update Simulation_Controller

--- src/main/java/blackholesimulation/view/controllers/SimulationController.java
+++ src/main/java/blackholesimulation/view/controllers/SimulationController.java
@@ -1,28 +1,37 @@
 package blackholesimulation.view.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-/**
- *
- * @author m_vit
- */
+
 public class SimulationController implements Initializable {
     
     @FXML
     private Label label;
+ 
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    @FXML private void back(ActionEvent event) throws IOException {
+        
+        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("/Views/Inicial_View.fxml"));
+       
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Load fxml on scene
+        stage.setTitle("Info");
+        stage.setScene(new Scene(fxmlloader.load()));
+        stage.show();
     }
-    
-    @Override
+    @Override 
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
